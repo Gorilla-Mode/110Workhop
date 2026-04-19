b74901cedb8b2a14dea47b05094cae96158594a7
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-04-19 20:33:18 +0200

    enum example

---
+++ Eksempler/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Eksempler;
+
+enum MenuOption
+{
+    Exit,   //0
+    Add,    //1
+    Remove, //2
+    List    //3
+}
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(MenuOption.Exit);     // -> Exit
+        Console.WriteLine((int)MenuOption.Add); // -> 1
+
+        Enum.TryParse(Console.ReadLine(), out MenuOption choice);
+
+        switch (choice)
+        {
+            case MenuOption.Exit:   Console.WriteLine("Exiting...");     break;
+            case MenuOption.Add:    Console.WriteLine("Adding...");      break;
+            case MenuOption.Remove: Console.WriteLine("Removing...");    break;
+            case MenuOption.List:   Console.WriteLine("Listing...");     break;
+            default:                Console.WriteLine("Invalid choice"); break;
+        }
+    }
+}
