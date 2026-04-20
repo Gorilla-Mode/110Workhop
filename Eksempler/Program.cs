cfb1b17c93858af708b70920c4614c5e6b7e5e89
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-04-20 02:53:30 +0200

    string swithc example

--- Eksempler/Program.cs
+++ Eksempler/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Eksempler;
+﻿using System.Globalization;
+
+namespace Eksempler;
 
 enum MenuOption
 {
@@ -8,6 +10,19 @@ enum MenuOption
     List    //3
 }
 
+class Person
+{
+    public string Name { get; set; }
+    
+    public Person(string name)
+    {
+        if (name.Contains("Walter", StringComparison.InvariantCultureIgnoreCase))
+            throw new ArgumentException("He's the devil", nameof(name));
+        
+        Name = name;
+    }
+}
+
 static class Program
 {
     public static void Main(string[] args)
@@ -15,7 +30,8 @@ static class Program
         Console.WriteLine(MenuOption.Exit);     // -> Exit
         Console.WriteLine((int)MenuOption.Add); // -> 1
 
-        Enum.TryParse(Console.ReadLine(), out MenuOption choice);
+        if (!Enum.TryParse(Console.ReadLine(), out MenuOption choice))
+            return; 
 
         switch (choice)
         {
@@ -24,6 +40,33 @@ static class Program
             case MenuOption.Remove: Console.WriteLine("Removing...");    break;
             case MenuOption.List:   Console.WriteLine("Listing...");     break;
             default:                Console.WriteLine("Invalid choice"); break;
+        }
+
+        Console.WriteLine("Enter name: ");
+        var name = Console.ReadLine() ?? string.Empty;
+        
+        var person = new Person(name);
+
+        Console.WriteLine("Todo List\n 1. Add task\n 2. Select task\n 3. List Items\n 0. Exit");
+        var input = Console.ReadLine() ?? string.Empty;
+
+        switch (input)
+        {
+            case "1":
+                Console.WriteLine("Adding...");
+                break;
+            case "2 ":
+                Console.WriteLine("Selecting...");
+                break;
+            case "\u0033":
+                Console.WriteLine("Listing...");
+                break;
+            case "O":
+                Console.WriteLine("Exiting...");
+                break;
+            default:
+                Console.WriteLine("Invalid choice");
+                break;
         }
     }
 }
